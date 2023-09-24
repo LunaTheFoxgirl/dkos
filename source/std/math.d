dc08333eb5b9e4094f3ed55d5f0bf13abdba4608
LunaTheFoxgirl <cli-luna@protonmail.com>
2023-09-24 17:28:56 +0200

    Add more funcs to std.math

--- source/std/math.d
+++ source/std/math.d
@@ -1,29 +1,53 @@
 module std.math;
 import core.stdc.math;
 
+enum float PI = 3.14159;
+
+/// PI / 180 at compiletime, used for degrees/radians conversion.
+enum float PI_180 = PI / 180;
+
+/// 180 / PI at compiletime, used for degrees/radians conversion.
+enum float _180_PI = 180 / PI;
+
 float sin(float x) { return sinf(x); }
 float cos(float x) { return cos(x); }
 float tan(float x) { return tan(x); }
 float mod(float x, float y) { return fmod(x, y); }
 
+float floor(float x) { return core.stdc.math.floor(x); }
+float min(float x, float y) { return x < y ? x : y; }
+float max(float x, float y) { return x > y ? x : y; }
+float clamp(float x, float minV, float maxV) { return min(max(x, minV), maxV); }
+
 bool isNaN(float x) { return cast(bool)isnanf(x); }
 bool isInfinity(float x) { return cast(bool)isinf(x); }
 
-float[3] hsv2rgb(float h, float s, float v) {
-    if(s == 0.0f) { // s
-        return [v, v, v]; // v
-    } else {
-        float var_h = h * 6;
-        float var_i = floor(var_h);
-        float var_1 = v * (1 - s);
-        float var_2 = v * (1 - s * (var_h - var_i));
-        float var_3 = v * (1 - s * (1 - (var_h - var_i)));
 
-        if(var_i == 0.0f)      return [v, var_3, var_1];
-        else if(var_i == 1.0f) return [var_2, v, var_1];
-        else if(var_i == 2.0f) return [var_1, v, var_3];
-        else if(var_i == 3.0f) return [var_1, var_2, v];
-        else if(var_i == 4.0f) return [var_3, var_1, v];
-        else                   return [v, var_1, var_2];
-    }
+
+/// Converts degrees to radians.
+real radians(real degrees) {
+    return PI_180 * degrees;
+}
+
+/// Compiletime version of $(I radians).
+real cradians(real degrees)() {
+    return radians(degrees);
+}
+
+/// Converts radians to degrees.
+real degrees(real radians) {
+    return _180_PI * radians;
+}
+
+/// Compiletime version of $(I degrees).
+real cdegrees(real radians)() {
+    return degrees(radians);
+}
+
+/// Returns 0.0 if x <= edge0 and 1.0 if x >= edge1 and performs smooth 
+/// hermite interpolation between 0 and 1 when edge0 < x < edge1. 
+/// This is useful in cases where you would want a threshold function with a smooth transition.
+float smoothstep(float edge0, float edge1, float x) {
+    auto t = clamp((x - edge0) / (edge1 - edge0), 0, 1);
+    return t * t * (3 - 2 * t);
 }
